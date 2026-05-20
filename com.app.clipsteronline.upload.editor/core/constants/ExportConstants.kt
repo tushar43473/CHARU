7fa47a0cb919d0d81e9944f03f1eb3d6afde1f51
tushar43473 <ts7066654@gmail.com>
2026-05-20 15:44:40 +0530

    Implement centralized editor configuration and constants system

--- com.app.clipsteronline.upload.editor/core/constants/ExportConstants.kt
+++ com.app.clipsteronline.upload.editor/core/constants/ExportConstants.kt
@@ -1,3 +1,25 @@
 package com.app.clipsteronline.upload.editor.core.constants
 
-object ExportConstants { const val VERSION = 1 }
+object ExportConstants {
+    const val RES_720P = "1280x720"
+    const val RES_1080P = "1920x1080"
+    const val RES_2K = "2560x1440"
+    const val RES_4K = "3840x2160"
+
+    const val FPS_24 = 24
+    const val FPS_30 = 30
+    const val FPS_60 = 60
+
+    const val BITRATE_720P = 4_500_000
+    const val BITRATE_1080P = 8_000_000
+    const val BITRATE_2K = 16_000_000
+    const val BITRATE_4K = 35_000_000
+
+    const val CODEC_H264 = "h264"
+    const val CODEC_H265 = "h265"
+    const val CODEC_AV1 = "av1"
+
+    const val CONTAINER_MP4 = "mp4"
+    const val CONTAINER_MOV = "mov"
+    const val CONTAINER_MKV = "mkv"
+}
--- com.app.clipsteronline.upload.editor/core/constants/RenderConstants.kt
+++ com.app.clipsteronline.upload.editor/core/constants/RenderConstants.kt
@@ -1,3 +1,19 @@
 package com.app.clipsteronline.upload.editor.core.constants
 
-object RenderConstants { const val VERSION = 1 }
+object RenderConstants {
+    const val EGL_CONTEXT_CLIENT_VERSION = 3
+    const val ENABLE_HARDWARE_ACCELERATION = true
+
+    const val PREVIEW_RENDER_FPS = 30
+    const val HIGH_QUALITY_RENDER_FPS = 60
+
+    const val DEFAULT_TEXTURE_LIMIT = 2048
+    const val SAFE_TEXTURE_LIMIT = 4096
+    const val FLAGSHIP_TEXTURE_LIMIT = 8192
+
+    const val SHADER_CACHE_LIMIT = 128
+    const val SHADER_COMPILE_TIMEOUT_MS = 1200L
+
+    const val FRAME_QUEUE_SIZE = 3
+    const val RENDER_THREAD_PRIORITY = -4
+}
